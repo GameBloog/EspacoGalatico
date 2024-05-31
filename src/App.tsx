3892ce96325b1a1fd789df1c910e0fdca2a3513f
GameBloog <gimenezcavalcantepedro@gmail.com>
2024-05-30 23:05:34 -0300

    global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter } from "react-router-dom"
 import Rotas from "./Routes.jsx"
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default.js"
+import { GlobalStyle } from "./styles/global.js"
 // import "../global.css"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Rotas />
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Rotas />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
