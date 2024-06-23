96a3a7d4ea589635b74f4403f7223d88f8ae335e
root <root@DESKTOP-91QIKRC>
2024-06-23 16:48:43 -0300

    👌 MELHORIA: organization img

--- src/paginas/index.tsx
+++ src/paginas/index.tsx
@@ -10,11 +10,34 @@ import {
   ImgEspacofestas,
 } from "./styles.ts"
 
-import mesaDeJogos from "../assets/imagens/Mesas-de-jogos.jpg"
-import mesaRefeitorio from "../assets/imagens/Mesas-refeitorio.jpg"
-import espacoPrivativo from "../assets/imagens/Espaços-privativos.jpg"
-import pacotesEspeciais from "../assets/imagens/pacotes-especiais.jpg"
 import espacoFestas from "../assets/imagens/espaço-de-festas.jpg"
+
+import avaliacao1 from "../assets/imagens/clientOpnion/Imagem1.png"
+import avaliacao2 from "../assets/imagens/clientOpnion/Imagem2.png"
+import avaliacao3 from "../assets/imagens/clientOpnion/Imagem3.png"
+import avaliacao4 from "../assets/imagens/clientOpnion/Imagem4.png"
+import avaliacao5 from "../assets/imagens/clientOpnion/Imagem5.png"
+
+import mesaDeJogos from "../assets/imagens/games/Mesas-de-jogos.jpg"
+import pacotesEspeciais from "../assets/games/imagens/pacotes-especiais.jpg"
+import brinquedos from "../assets/imagens/games/Brinquedos.jpg"
+import brinquedos2 from "../assets/imagens/games/Brinquedos2.jpg"
+import brinquedos3 from "../assets/imagens/games/Brinquedos3.jpg"
+import brinquedos4 from "../assets/imagens/games/Brinquedos4.jpg"
+
+//Aguardar novas imgens
+import mesasRefeitorio from "../assets/imagens/Cardapio/visao-ampla.jpg"
+import camarinCardapio from "../assets/imagens/Cardapio/Camarin.jpg"
+import espacoCardapio from "../assets/imagens/Cardapio/Espaco2.jpg"
+
+import camarin from "../assets/imagens/espaco/Camarin.jpg"
+import espaco2 from "../assets/imagens/espaco/Espaco2.jpg"
+import espacoDeFestas from "../assets/imagens/espaco/espaço-de-festas.jpg"
+import visaoAmpla from "../assets/imagens/espaco/visao-ampla.jpg"
+
+import decoracao from "../assets/imagens/decoration/Decoracao.jpg"
+import fachada from "../assets/imagens/decoration/fachada.jpg"
+import mesas from "../assets/imagens/decoration/Mesas-refeitorio.jpg"
 
 import { CarouselwithText } from "../components/slideCardapio/index.tsx"
 import { Header } from "../components/Header/index.tsx"
@@ -28,11 +51,40 @@ import { LastPage } from "../components/LastPage/index.tsx"
 import { ButtonFixed } from "../components/ButtonFixed/index.tsx"
 import { EndPage } from "../components/Footer/styles.ts"
 
-const images = [
+const games = [
   { id: "1", image: mesaDeJogos },
-  { id: "2", image: mesaRefeitorio },
-  { id: "3", image: espacoPrivativo },
-  { id: "4", image: pacotesEspeciais },
+  { id: "2", image: pacotesEspeciais },
+  { id: "3", image: brinquedos },
+  { id: "4", image: brinquedos2 },
+  { id: "5", image: brinquedos3 },
+  { id: "6", image: brinquedos4 },
+]
+
+const cardapio = [
+  { id: "1", image: mesasRefeitorio },
+  { id: "2", image: camarinCardapio },
+  { id: "3", image: espacoCardapio },
+]
+
+const espaco = [
+  { id: "1", image: camarin },
+  { id: "2", image: espaco2 },
+  { id: "3", image: espacoDeFestas },
+  { id: "4", image: visaoAmpla },
+]
+
+const decoration = [
+  { id: "1", image: decoracao },
+  { id: "2", image: fachada },
+  { id: "3", image: mesas },
+]
+
+const clientOpnion = [
+  { id: "1", image: avaliacao1 },
+  { id: "2", image: avaliacao2 },
+  { id: "3", image: avaliacao3 },
+  { id: "4", image: avaliacao4 },
+  { id: "5", image: avaliacao5 },
 ]
 
 export function Home() {
@@ -69,20 +121,10 @@ export function Home() {
                   atividades interativas e criativas para manter a criançada
                   sempre entretida."
             title="Brinquedos e Atividades"
-            images={images}
-          />
-          <ImageWithTextSlide
-            description="Contamos com uma vasta seleção de brinquedos, incluindo
-                  labamba, brinquedão com enorme piscina de bolinhas, cama
-                  elástica, multijogos, tombo legal, air game, space game e
-                  muito mais. Além disso, nossas recreadoras promovem jogos e
-                  atividades interativas e criativas para manter a criançada
-                  sempre entretida."
-            title="Brinquedos e Atividades"
-            images={images}
+            images={games}
           />
 
-          <Cardapio images={images} />
+          <Cardapio images={cardapio} />
 
           <SingleWords text=" Temos opções para diferentes tamanhos de festas e orçamentos." />
 
@@ -96,7 +138,7 @@ export function Home() {
                   proporcionando todo conforto e organização necessárias para
                   que sua festa seja incrível."
             title="Espaço"
-            images={images}
+            images={espaco}
           />
 
           <ImageWithTextSlide
@@ -106,7 +148,7 @@ export function Home() {
                   fazemos uma entrada muito especial com música, iluminação e
                   máquina de fumaça."
             title="Decorações"
-            images={images}
+            images={decoration}
           />
 
           <SingleWords text="Confira nossos pacotes e garanta sua festa completa." />
@@ -128,7 +170,7 @@ export function Home() {
             <img src={espacoFestas} alt="" />
           </ImgEspacofestas>
 
-          <ClientsOpnion images={images} />
+          <ClientsOpnion images={clientOpnion} />
         </MidleDiv>
 
         <Section>
